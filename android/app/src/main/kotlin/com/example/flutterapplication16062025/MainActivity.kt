800c03cde271a10553f542af82d87de68c4c3b96
IT <pradeep@tcschennai.com>
2025-06-17 15:17:25 +0530

    first committ

---
+++ android/app/src/main/kotlin/com/example/flutterapplication16062025/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterapplication16062025
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS/macOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  final controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..setNavigationDelegate(
+  NavigationDelegate(
+  onProgress: (int progress) {
+  // Update loading bar.
+  },
+  onPageStarted: (String url) {},
+  onPageFinished: (String url) {},
+  onHttpError: (HttpResponseError error) {},
+  onWebResourceError: (WebResourceError error) {},
+  onNavigationRequest: (NavigationRequest request) {
+  if (request.url.startsWith('https://www.youtube.com/')) {
+  return NavigationDecision.prevent;
+  }
+  return NavigationDecision.navigate;
+  },
+  ),
+  )
+  ..loadRequest(Uri.parse('https://hrms.madrassilks.com'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
